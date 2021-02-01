7b9a8cd2ce9683222494ad61f7ccb2b17f676da3
JBSilcott <silcott.jb@gmail.com>
2021-01-31 19:46:46 -0500

    update

--- AscentApp/src/app/app.component.ts
+++ AscentApp/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AscentApp';
+export class AppComponent implements OnInit {
+  title = "App";
+
+  constructor() { }
+
+ 
+  public loadJsFile(url: string) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
+
+  ngOnInit() {
+    this.loadJsFile("./src/assets/js/breakpoints.min.js"),
+      this.loadJsFile("./src/assets/js/browser.min.js"),
+      this.loadJsFile("./src/assets/js/jquery.dropotron.min.js"),
+      this.loadJsFile("./src/assets/js/jquery.min.js"),
+      this.loadJsFile("./src/assets/js/jquery.scrollex.min.js"),
+      this.loadJsFile("./src/assets/js/main.js"),
+      this.loadJsFile("./src/assets/js/util.js")
+      ;
+  }
+
 }
+  
+
